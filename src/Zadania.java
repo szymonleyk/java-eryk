c99abf7555506d7b2e0873b2ac47bd9111fa1cb2
Szymon Leyk <biuro@szymonleyk.pl>
2021-04-16 08:20:04 +0200

    #5 zadanie2

--- src/Zadania.java
+++ src/Zadania.java
@@ -3,28 +3,75 @@ public class Zadania {
 
         // zadanie 1
         int a = 100;
-        while (a>=1) {
+        while (a >= 1) {
             System.out.println(a);
             a--;
         }
 
         // zadanie2
-        char[] imie = {'c','z','e','s','ł','a','w','a'};
-        System.out.print(imie[0]);
-        System.out.print(imie[1]);
-        System.out.print(imie[2]);
-        System.out.print(imie[3]);
-        System.out.print(imie[4]);
-        System.out.print(imie[5]);
-        System.out.print(imie[6]);
-
-        System.out.println();
+        char[] imie = {'c', 'z', 'e', 's', 'ł', 'a', 'w', 'a'};
+//        System.out.print(imie[0]);
+//        System.out.print(imie[1]);
+//        System.out.print(imie[2]);
+//        System.out.print(imie[3]);
+//        System.out.print(imie[4]);
+//        System.out.print(imie[5]);
+//        System.out.print(imie[6]);
+//
+//        System.out.println();
 
 
         int b = 0;
-        while(b<imie.length) {
+        while (b < imie.length) {
             System.out.print(imie[b]);
             b++;
         }
+        System.out.println();
+
+        b = 0;
+        while (b < imie.length) {
+            System.out.print(imie[b]);
+            b++;
+        }
+        System.out.println();
+
+        b = 0;
+        while (b < imie.length) {
+            System.out.print(imie[b]);
+            b++;
+        }
+        System.out.println();
+
+        b = 0;
+        while (b < imie.length) {
+            System.out.print(imie[b]);
+            b++;
+        }
+        System.out.println();
+
+        b = 0;
+        while (b < imie.length) {
+            System.out.print(imie[b]);
+            b++;
+        }
+        System.out.println();
+
+
+        System.out.println("------------------");
+        // pętlą
+
+        int ileRazy = 0;
+        while (ileRazy < 500) {
+            b = 0;
+            while (b < imie.length) {
+                System.out.print(imie[b]);
+                b++;
+            }
+            System.out.println();
+            ileRazy++;
+        }
+
+        
+
     }
 }
